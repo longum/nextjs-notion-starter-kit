ff59be4ef0c3f6dc24200f97b1ef5243b146c2d2
longum <sunbingyan@hotmail.com>
2022-03-18 15:31:52 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'a5ae9a09f6654b2b88c63f344333f9e0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '果果的成长记录',
+  domain: 'https://www.notion.so/a5ae9a09f6654b2b88c63f344333f9e0',
+  author: 'Brigsby',
 
   // open graph metadata (optional)
   description: 'Example site description',
